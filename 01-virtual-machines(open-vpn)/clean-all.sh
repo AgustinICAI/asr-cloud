aa7b246069249de6f3cd4158aca2c5dec98641d8
AgustinICAI <a.g.lamela@gmail.com>
2022-09-18 10:34:43 +0200

    subiendo practica 3

--- 01-virtual-machines(open-vpn)/clean-all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gcloud compute addresses delete uk-vpn-ip --region europe-west2
-gcloud compute instances delete vpn-instance --zone europe-west2-a
-gcloud compute firewall-rules delete london-vpn-udp
